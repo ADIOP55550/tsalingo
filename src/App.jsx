12b589c7fc22d4d4038782a2674bf2f04174ccfb
GigaNByte <karol.saj0@gmail.com>
2024-05-19 08:19:06 +0200

    app.jsx quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import WhereYouCameFrom from "./views/WhereYouCameFrom.jsx"
 import Quiz from "./views/Quiz.jsx"
 import PassportChallenge from "./views/PassportChallenge.jsx"
 import Navbar from './components/Navbar.jsx'
-import ThemeSelect from './components/ThemeSelect.jsx'
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
       <div className="mockup-phone">
         <div className="camera"></div>
         <div className="display relative">
-
           <div className="artboard artboard-demo phone-1 block  bg-neutral">
             <div className="container flex  h-full">
               <div className="container h-full flex flex-col">
@@ -34,41 +32,23 @@ function App() {
                   </Routes >
                 </div>
               </div>
-
-              <div className="artboard artboard-demo phone-1 block  bg-neutral">
-                <div className="container flex  h-full">
-                  <div className="container h-full flex flex-col">
-                    <Navbar />
-                    <div className='h-full flex px-4 justify-center'>
-                      <Routes >
-                        <Route exact path="/" element={<Home />} />
-                        <Route path="/whereYouCameFrom" element={<WhereYouCameFrom />} />
-                        <Route path="/levelSelect" element={<LevelSelect />} />
-                        <Route path="/quiz/:levelName" element={<Quiz />} />
-                        <Route path="/passportChallenge" element={<PassportChallenge />} />
-                        <Route path="/itsDone" element={<EagleFinalMessage />} />
-                        <Route path="/couponMessage" element={<CouponMessage />} />
-                      </Routes >
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
-          <div>
-            <ul className="menu bg-base-200 w-56 rounded-box">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/whereYouCameFrom">Where You Came From</Link></li>
-              <li><Link to="/levelSelect">Level Select</Link></li>
-              <li><Link to="/quiz">Quiz</Link></li>
-              <li><Link to="/passportChallenge">Passport Challenge</Link></li>
-              <li><Link to="/itsDone">Eagle Final Message</Link></li>
-              <li><Link to="/couponMessage">Coupon Message</Link></li>
-            </ul>
-          </div>
-          <ThemeSelect />
-        </Router>
-        );
+        </div>
+      </div>
+      <div>
+        <ul className="menu bg-base-200 w-56 rounded-box">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/whereYouCameFrom">Where You Came From</Link></li>
+          <li><Link to="/levelSelect">Level Select</Link></li>
+          <li><Link to="/quiz">Quiz</Link></li>
+          <li><Link to="/passportChallenge">Passport Challenge</Link></li>
+          <li><Link to="/itsDone">Eagle Final Message</Link></li>
+          <li><Link to="/couponMessage">Coupon Message</Link></li>
+        </ul>
+      </div>
+    </Router>
+  );
 }
 
-        export default App
+export default App
