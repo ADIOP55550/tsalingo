a1b769929a621efb9ce9221dfb73f33053a316f0
GigaNByte <karol.saj0@gmail.com>
2024-05-19 12:02:42 +0200

    added quiz

--- src/components/AnimatedQuizCard.jsx
+++ src/components/AnimatedQuizCard.jsx
@@ -1,10 +1,9 @@
-// src/components/AnimatedQuizCard.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
-import { useDrag } from 'react-use-gesture';
+import { useGesture } from 'react-use-gesture';
 import QuizCard from './QuizCard';
 
-function AnimatedQuizCard({ image, onDrop }) {
+function AnimatedQuizCard({ image }) {
     const [props, set] = useSpring(() => ({
         x: 0,
         y: 0,
@@ -13,35 +12,109 @@ function AnimatedQuizCard({ image, onDrop }) {
         config: { tension: 300, friction: 30 },
     }));
 
-    const bind = useDrag(({ down, movement: [mx, my] }) => {
-        set({ x: down ? mx : 0, y: down ? my : 0, rotateZ: down ? mx / 10 : 0 }),
-    });
+    const [dropZone, setDropZone] = useState(null);
 
-    const handleDrop = () => {
-        console.log('los');
-        if (onDrop) {
-            onDrop({ image });
-        }
+    const handleDrop = (event) => {
+        event.preventDefault();
+        const zone = event.target.id;
+        setDropZone(zone);
     };
 
+    const [thisDisplay, setThisDisplay] = useState('block');
+    const [thisClass, setThisClass] = useState('green-overlay-fade-in');
+
+    const bind = useGesture({
+        onDrag: ({ active, movement: [x, y] }) => {
+            set({ x, y, scale: active ? 1.2 : 1 });
+        },
+        onDragEnd: ({ xy }) => {
+            console.log(xy);
+            console.log("test");
+            const elements = document.elementsFromPoint(xy[0], xy[1]);
+            console.log(elements);
+
+            let blindzone = false;
+
+            // Check if any element has both 'elem.id take' and 'elem.id leave'
+            for (const elem of elements) {
+                if (elem.id.includes('take') && elem.id.includes('leave')) {
+                    blindzone = true;
+                    break;
+                }
+            }
+            //check if array has elem.id take and elem.id leave both 
+
+            let foundDropZone = false;
+            elements.forEach((elem) => {
+                if (!blindzone) {
+                    if (elem.id === 'take') {
+                        console.log('Dropped on: #take');
+                        setDropZone('take');
+                        //create red animation with green color overlay and make display none
+                        setThisClass('green-overlay fade-out');
+
+                        setTimeout(() => {
+                            setThisDisplay('none');
+                        }, 200);
+
+
+
+                        foundDropZone = true;
+                    } else if (elem.id === 'leave') {
+                        console.log('Dropped on: #leave');
+                        setDropZone('leave');
+                        //create fadeout animation with green color overlay and make display none
+                        setThisClass('green-overlay fade-out');
+
+                        setTimeout(() => {
+                            setThisDisplay('none');
+                        }, 200);
+
+
+                        foundDropZone = true;
+                    }
+                }
+
+            });
+
+
+
+        },
+    });
+
+    // Listen for the drop event on the document
+    React.useEffect(() => {
+        document.addEventListener('drop', handleDrop);
+        return () => {
+            document.removeEventListener('drop', handleDrop);
+        };
+    }, []);
+
     return (
-        <animated.div
-            {...bind()}
-            onDrop={handleDrop} // Ensure the handleDrop function is called on drop
-            onDragOver={(e) => e.preventDefault()} // Necessary for onDrop to work
-            style={{
-                transform: props.x.to((x) => `translateX(${x}px)`)
-                    .to((x) => `${x} translateY(${props.y.get()}px)`)
-                    .to((x) => `${x} rotate(${props.rotateZ.get()}deg)`),
-                width: '200px', // Set the width to appropriate value
-                height: '300px', // Set the height to appropriate value
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <QuizCard image={image} />
-        </animated.div>
+
+        <div style={{ display: thisDisplay, position: 'relative' }} className={thisClass}>
+            <animated.div
+                {...bind()}
+                style={{
+                    transform: props.x
+                        .to((x) => `translateX(${x}px)`)
+                        .to((x) => `${x} translateY(${props.y.get()}px)`)
+                        .to((x) => `${x} rotate(${props.rotateZ.get()}deg)`),
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <QuizCard image={image} />
+            </animated.div>
+            {
+                dropZone && (
+                    <p>
+                        Dropped {image} in {dropZone}
+                    </p>
+                )
+            }
+        </div >
     );
 }
 
--- src/views/Quiz.jsx
+++ src/views/Quiz.jsx
@@ -1,44 +1,32 @@
 import { useParams } from 'react-router-dom';
-import DraggableQuizCard from '../components/DraggableQuizCard';
 import AnimatedQuizCard from '../components/AnimatedQuizCard';
-import DropZone from '../components/DropZone';
 
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 export default function Quiz() {
     const { levelName } = useParams();
 
-    const handleDrop = (item, zone) => {
-        console.log(`Dropped ${item.image} in ${zone}`);
-    };
-
     return (
-        <DndProvider backend={HTML5Backend}>
-            <div className="h-full w-full">
-                <div className="mt-4 flex justify-center">
-                    <h1>Quiz for {levelName}</h1>
+        <div className="h-full w-full">
+            <div className="mt-4 flex justify-center">
+                <h1>Quiz for {levelName}</h1>
+            </div>
+            <div className="flex w-full -mx-4 h-2/3">
+                <div id="take" className="w-full  flex-1/2 h-full justify-left align-center pt-5 mr-2">
+                    <div className="bg-indigo-500 w-40 h-40 block"></div>
+                    <p className="pl-5">...Take it</p>
                 </div>
-                <div className="flex w-full -mx-4 h-2/3">
-                    <DropZone id="take" onDrop={handleDrop}>
-                        <div className="w-1/2 h-full pr-5 justify-left align-center pt-5">
-                            <div className="bg-indigo-500 w-40 h-40 block"></div>
-                            <p className="pl-5">...Take it</p>
-                        </div>
-                    </DropZone>
-                    <DropZone id="leave" onDrop={handleDrop}>
-                        <div className="w-1/2 h-full pl-5 justify-right align-center text-right pt-5">
-                            <div className="bg-red-500 w-40 h-40 block"></div>
-                            <p>Leave it...</p>
-                        </div>
-                    </DropZone>
+                <div id="leave" className="w-full flex-1/2 h-full  justify-right align-center text-right pt-5 ">
+                    <div className="bg-red-500 w-40 h-40 block "></div>
+                    <p className="pr-4">Leave it...</p>
                 </div>
 
             </div>
-            <div className="flex justify-center align-center">
-                {/* <DraggableQuizCard image={levelName} />*/}
+            <div className="flex justify-center align-center h-1/3">
+                <AnimatedQuizCard image={levelName} />
                 <AnimatedQuizCard image={levelName} />
             </div>
-        </DndProvider>
+        </div>
+
+
     );
 }
